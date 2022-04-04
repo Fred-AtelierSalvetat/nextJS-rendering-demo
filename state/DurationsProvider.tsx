863ff93609218f86c4025e3bc748e38ada817c53
Frederic Salvetat <frederic@ateliersalvetat.com>
2022-04-04 12:28:57 +0200

    Add createContext explicit default value

--- state/DurationsProvider.tsx
+++ state/DurationsProvider.tsx
@@ -1,19 +1,19 @@
 import { createContext, useContext, useState } from "react";
 
-// Create Context object.
-const DurationsContext = createContext();
-
 export interface IStore {
-  csr: [number | undefined, (value?: number) => void];
-  ssr: [number | undefined, (value?: number) => void];
-  ssg: [number | undefined, (value?: number) => void];
+  csr: [DOMHighResTimeStamp | undefined, (value?: DOMHighResTimeStamp) => void];
+  ssr: [DOMHighResTimeStamp | undefined, (value?: DOMHighResTimeStamp) => void];
+  ssg: [DOMHighResTimeStamp | undefined, (value?: DOMHighResTimeStamp) => void];
 }
+
+// Create Context object.
+const DurationsContext = createContext(undefined);
 
 // Export Provider.
 export function DurationsProvider({ children }) {
-  const [csrDuration, setCsrDuration] = useState();
-  const [ssrDuration, setSsrDuration] = useState();
-  const [ssgDuration, setSsgDuration] = useState();
+  const [csrDuration, setCsrDuration] = useState<DOMHighResTimeStamp>();
+  const [ssrDuration, setSsrDuration] = useState<DOMHighResTimeStamp>();
+  const [ssgDuration, setSsgDuration] = useState<DOMHighResTimeStamp>();
 
   const store: IStore = {
     csr: [csrDuration, setCsrDuration],
